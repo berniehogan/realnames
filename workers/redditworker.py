1e41b3b12923343e9a21dc13c504ec88a439636e
berniehogan <bernie.hogan@oii.ox.ac.uk>
2014-05-01 14:10:10 +0100

    initial folder setup

---
+++ workers/redditworker.py
@@ -0,0 +1,12 @@
+#!/user/bin/python
+
+# redditworker.py 
+
+""" This is the primasry reddit worker that will be used to capture comments
+from key subgroups on reddit as specified in a subgroups file. """
+
+__author__        = "Bernie Hogan"
+__copyright__     = "Copyright 2014, University of Oxford"
+
+
+import praw
---
+++ workers/youtubeworker.py
@@ -0,0 +1,12 @@
+#!/user/bin/python
+
+# youtubeworker.py 
+
+""" This is the primary youtube worker that will be used to capture comments
+from key subgroups on reddit as specified in a subgroups file. """
+
+__author__        = "Bernie Hogan"
+__copyright__     = "Copyright 2014, University of Oxford"
+
+
+import praw
